2fcd5bd13919ee850789c82f7e6ec648513b04b7
Zainnnnnnnn <scavanger9898@gmail.com>
2025-10-21 14:39:28 +0700

    Extended Vignere update

--- Fungsi.js
+++ Fungsi.js
@@ -152,16 +152,18 @@ const UI = {
     if (rawText !== undefined){
       UI.lastRawText = rawText;
       rawBox.textContent = "RAW: " + rawText;
-      const b64 = Utils.toB64(rawText).replace(/\s+/g,'').toLowerCase();
+      const b64 = Utils.toB64(rawText).replace(/\s+/g,'');
       UI.lastB64 = b64;
       b64Box.textContent = "BASE64: " + b64;
       UI.lastBytes = new TextEncoder().encode(rawText);
     }
     if (bytes instanceof Uint8Array){
       UI.lastBytes = bytes;
-      const b64 = Utils.toB64(bytes).replace(/\s+/g,'').toLowerCase();
+      const decoded = Utils.fromBytes(bytes);
+      UI.lastRawText = decoded;
+      rawBox.textContent = "RAW: " + decoded;
+      const b64 = Utils.toB64(bytes).replace(/\s+/g,'');
       UI.lastB64 = b64;
-      rawBox.textContent = "BYTES: ["+ bytes.length +" byte]";
       b64Box.textContent = "BASE64: " + b64;
     }
   },
